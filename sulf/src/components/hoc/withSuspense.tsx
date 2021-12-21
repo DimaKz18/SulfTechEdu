c2b286c541ca4fecf638fd9248e34bd40fe75286
nuznhy <nuznhy@gmail.com>
2021-12-21 23:09:22 +0200

    sulf removed

--- sulf/src/components/hoc/withSuspense.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const withSuspense = (Component: React.ComponentType) => {
-    return (props: any) => {
-        return <React.Suspense fallback={<div>Loading..</div>}>
-            <Component {...props}/>
-        </React.Suspense>
-    };
-}
